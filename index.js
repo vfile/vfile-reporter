8cf495a391d234a55503d8d05490253721f5df55
Titus Wormer <tituswormer@gmail.com>
2015-08-17 12:05:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,249 @@
+/**
+ * @author Titus Wormer
+ * @author Sindre Sorhus
+ * @copyright 2015 Titus Wormer
+ * @copyright 2013 Nicholas C. Zakas
+ * @license MIT
+ * @module vfile:reporter
+ * @fileoverview Stylish reporter for virtual files.
+ */
+
+'use strict';
+
+/*
+ * Dependencies.
+ */
+
+var pluralize = require('plur');
+var chalk = require('chalk');
+var table = require('text-table');
+var repeat = require('repeat-string');
+var sort = require('vfile-sort');
+
+/*
+ * List of probabbly lengths of messages.
+ */
+
+var POSITION_LENGTH = '00:0-00:0'.length;
+var LABEL_LENGTH = 'message'.length;
+var MESSAGE_LENGTH = 'this is an average message'.length;
+
+/**
+ * Reject messages without `fatal: true`.
+ *
+ * NOTE: Modifies the given files.
+ *
+ * @param {Array.<VFile>} files - List of files.
+ */
+function removeNonFatalMessages(files) {
+    files.forEach(function (file) {
+        file.messages = file.messages.filter(function (message) {
+            return message.fatal === true;
+        })
+    });
+}
+
+/**
+ * Reject files without messages.
+ *
+ * @param {Array.<VFile>} files - List of files.
+ * @return {Array.<VFile>} - `files` without non-failed messages.
+ */
+function removeNonFailedFiles(files) {
+    return files.filter(function (file) {
+        return Boolean(file.messages.length);
+    });
+}
+
+/**
+ * Get the length of `value`, ignoring ANSI sequences.
+ *
+ * @param {string} value - Value to `pad`.
+ * @return {number} - Length of `value`.
+ */
+function realLength(value) {
+    var index;
+
+    value = chalk.stripColor(value);
+    index = value.indexOf('\n');
+
+    if (index !== -1) {
+        value = value.slice(0, index);
+    }
+
+    return value.length;
+}
+
+/**
+ * Pad `value` on the `side` (where truthy means left and
+ * falsey means right).
+ *
+ * @param {string} value - Value to `pad`.
+ * @param {number} minimum - Pad to `minimum`.
+ * @param {boolean?} [side] - Side to pad on.
+ * @return {string} - Right-padded `value`.
+ */
+function pad(value, minimum, side) {
+    var padding = repeat(' ', minimum - realLength(value));
+    return side ? padding + value : value + padding;
+}
+
+/**
+ * Pad `value` on the left.
+ *
+ * @param {string} value - Value to `pad`.
+ * @param {number} minimum - Pad to `minimum`.
+ * @return {string} - Left-padded `value`.
+ */
+function padLeft(value, minimum) {
+    return pad(value, minimum, true);
+}
+
+/**
+ * Pad `value` on the right.
+ *
+ * @param {string} value - Value to `pad`.
+ * @param {number} minimum - Pad to `minimum`.
+ * @return {string} - Right-padded `value`.
+ */
+function padRight(value, minimum) {
+    return pad(value, minimum, false);
+}
+
+/**
+ * @param {VFile|Array.<VFile>} files - One or more virtual
+ *   files.
+ * @param {Object} [options] - Configuration.
+ * @param {Object} [options.quiet=false] - Do not output
+ *   anything for a file which has no messages. The default
+ *   behaviour is to show a success message.
+ * @param {Object} [options.silent=false] - Do not output
+ *   messages without `fatal` set to true. Also sets
+ *   `quiet` to `true`.
+ * @return {string} - Formatted files.
+ */
+function reporter(files, options) {
+    var total = 0;
+    var errors = 0;
+    var warnings = 0;
+    var result = [];
+    var summaryColor;
+    var listing = false;
+
+    if (!files) {
+        return '';
+    }
+
+    if (!('length' in files)) {
+        files = [files];
+    }
+
+    if (!options) {
+        options = {};
+    }
+
+    if (options.silent) {
+        removeNonFatalMessages(files);
+    }
+
+    if (options.silent || options.quiet) {
+        files = removeNonFailedFiles(files);
+    }
+
+    files.forEach(function (file, position) {
+        var filePath = file.filePath();
+        var name = filePath || '<stdin>';
+        var output = '';
+        var messages;
+        var fileColor;
+
+        sort(file);
+
+        messages = file.messages;
+
+        total += messages.length;
+
+        messages = messages.map(function (message) {
+            var color = 'yellow';
+            var location = message.name;
+            var label;
+            var reason;
+
+            if (filePath) {
+                location = location.slice(location.indexOf(':') + 1);
+            }
+
+            if (message.fatal) {
+                color = fileColor = summaryColor = 'red';
+                label = 'error';
+                errors++;
+            } else if (message.fatal === false) {
+                label = 'warning';
+                warnings++;
+
+                if (!summaryColor) {
+                    summaryColor = color;
+                }
+
+                if (!fileColor) {
+                    fileColor = color;
+                }
+            } else {
+                label = 'message';
+                color = 'gray';
+            }
+
+            reason = String(message);
+
+            if (message.stack.slice(0, reason.length) === reason) {
+                reason = message.message;
+            } else {
+                reason = message.stack;
+            }
+
+            return [
+                '',
+                padLeft(location, POSITION_LENGTH),
+                padRight(chalk[color](label), LABEL_LENGTH),
+                padRight(reason, MESSAGE_LENGTH),
+                message.ruleId || ''
+            ];
+        });
+
+        if (listing || (messages.length && position !== 0)) {
+            output += '\n';
+        }
+
+        output += chalk.underline[fileColor || 'green'](name);
+
+        listing = Boolean(messages.length);
+
+        if (!listing) {
+            output += ': no issues found';
+        } else {
+            output += '\n' + table(messages, {
+                align: ['', 'l', 'l', 'l'],
+                stringLength: realLength
+            });
+        }
+
+        result.push(output);
+    });
+
+    if (errors || warnings) {
+        result.push(
+            '\n' + chalk[summaryColor].bold([
+            '\u2716 ', total, pluralize(' message', total),
+            ' (', errors, pluralize(' error', errors), ', ',
+            warnings, pluralize(' warning', warnings), ')'
+        ].join('')));
+    }
+
+    return result.length ? '\n' + result.join('\n') : '';
+}
+
+/*
+ * Expose.
+ */
+
+module.exports = reporter;
---
+++ test.js
@@ -0,0 +1,262 @@
+'use strict';
+
+/* eslint-env mocha */
+
+/*
+ * Dependencies.
+ */
+
+var assert = require('assert');
+var chalk = require('chalk');
+var vfile = require('vfile');
+var toVFile = require('to-vfile');
+var reporter = require('./index.js');
+
+/*
+ * Methods.
+ */
+
+var equal = assert.strictEqual;
+
+/**
+ * Helper to remove trailing white-space and ANSI-colors.
+ *
+ * @param {string} value - Value to clean.
+ * @return {string} - Cleaned `value`.
+ */
+function clean(value) {
+    return chalk.stripColor(value).trim();
+}
+
+/*
+ * Fixture of a `real` error.
+ */
+
+var exception;
+
+try {
+    /* eslint-disable no-undef */
+    variable;
+} catch (err) {
+    err.stack = err.stack
+        .replace(/\(\/.+\//g, '(')
+        .replace(/\d+:\d+/g, '1:1')
+        .split('\n')
+        .slice(0, 3)
+        .join('\n');
+
+    exception = err;
+}
+
+/*
+ * Tests.
+ */
+
+describe('vfile-reporter', function () {
+    it('should return empty when not given files', function () {
+        equal(reporter([]), '');
+        equal(reporter(), '');
+    });
+
+    it('should work on a single file', function () {
+        equal(clean(reporter(toVFile('a.js'))), 'a.js: no issues found');
+    });
+
+    it('should work without file-paths', function () {
+        equal(clean(reporter(vfile())), '<stdin>: no issues found');
+    });
+
+    it('should work on files without messages', function () {
+        equal(clean(reporter([
+            toVFile('a.js'),
+            toVFile('b.js'),
+            toVFile('c.js')
+        ])), [
+            'a.js: no issues found',
+            'b.js: no issues found',
+            'c.js: no issues found'
+        ].join('\n'));
+    });
+
+    it('should work on files with messages', function () {
+        var file = toVFile('a.js');
+
+        file.messages.push(file.message('Note!'));
+
+        equal(clean(reporter([
+            file,
+            toVFile('b.js'),
+            toVFile('c.js')
+        ])), [
+            'a.js',
+            '        1:1  message  Note!',
+            '',
+            'b.js: no issues found',
+            'c.js: no issues found'
+        ].join('\n'));
+    });
+
+    it('should work on files with warnings', function () {
+        var file = toVFile('a.js');
+
+        file.warn('Warning!');
+
+        equal(clean(reporter([
+            file,
+            toVFile('b.js'),
+            toVFile('c.js')
+        ])), [
+            'a.js',
+            '        1:1  warning  Warning!',
+            '',
+            'b.js: no issues found',
+            'c.js: no issues found',
+            '',
+            '✖ 1 message (0 errors, 1 warning)'
+        ].join('\n'));
+    });
+
+    it('should work on files with errors', function () {
+        var file = toVFile('a.js');
+
+        file.quiet = true;
+
+        file.fail('Error!');
+
+        equal(clean(reporter([
+            file,
+            toVFile('b.js'),
+            toVFile('c.js')
+        ])), [
+            'a.js',
+            '        1:1  error    Error!',
+            '',
+            'b.js: no issues found',
+            'c.js: no issues found',
+            '',
+            '✖ 1 message (1 error, 0 warnings)'
+        ].join('\n'));
+    });
+
+    it('should work on files with multiple mixed messages', function () {
+        var file = toVFile('a.js');
+
+        file.quiet = true;
+
+        file.fail('Error!');
+        file.messages.push(file.message('Note!'));
+        file.warn('Warning!');
+        file.warn('Another warning!');
+        file.messages.push(file.message('Another note!'));
+        file.fail('Another error!');
+
+        equal(clean(reporter(file)), [
+            'a.js',
+            '        1:1  error    Error!',
+            '        1:1  message  Note!',
+            '        1:1  warning  Warning!',
+            '        1:1  warning  Another warning!',
+            '        1:1  message  Another note!',
+            '        1:1  error    Another error!',
+            '',
+            '✖ 6 messages (2 errors, 2 warnings)'
+        ].join('\n'));
+    });
+
+    it('should support a single position', function () {
+        var file = vfile();
+
+        file.warn('Warning!', {
+            'line': 3,
+            'column': 2
+        });
+
+        equal(clean(reporter(file)), [
+            '<stdin>',
+            '        3:2  warning  Warning!',
+            '',
+            '✖ 1 message (0 errors, 1 warning)'
+        ].join('\n'));
+    });
+
+    it('should support a location', function () {
+        var file = vfile();
+
+        file.warn('Warning!', {
+            'start': {
+                'line': 3,
+                'column': 2
+            },
+            'end': {
+                'line': 4,
+                'column': 8
+            }
+        });
+
+        equal(clean(reporter(file)), [
+            '<stdin>',
+            '    3:2-4:8  warning  Warning!',
+            '',
+            '✖ 1 message (0 errors, 1 warning)'
+        ].join('\n'));
+    });
+
+    it('should support a “real” error (show a stack)', function () {
+        var file = toVFile('test.js');
+
+        file.quiet = true;
+        file.fail(exception);
+
+        equal(clean(reporter(file)), [
+            'test.js',
+            '        1:1  error    ReferenceError: variable is not defined',
+            '    at Object.<anonymous> (test.js:1:1)',
+            '    at Module._compile (module.js:1:1)',
+            '',
+            '✖ 1 message (1 error, 0 warnings)'
+        ].join('\n'));
+    });
+
+    it('should ignore successful files in `quiet` mode', function () {
+        var a = toVFile('a.js');
+        var b = toVFile('b.js');
+        var c = toVFile('c.js');
+
+        a.quiet = true;
+
+        a.fail('Error!');
+        b.warn('Warning!');
+
+        equal(clean(reporter([a, b, c], {
+            'quiet': true
+        })), [
+            'a.js',
+            '        1:1  error    Error!',
+            '',
+            'b.js',
+            '        1:1  warning  Warning!',
+            '',
+            '✖ 2 messages (1 error, 1 warning)'
+        ].join('\n'));
+    });
+
+    it('should ignore non-failures in `silent` mode', function () {
+        var a = toVFile('a.js');
+        var b = toVFile('b.js');
+        var c = toVFile('c.js');
+
+        a.quiet = true;
+
+        a.fail('Error!');
+        b.warn('Warning!');
+
+        equal(clean(reporter([a, b, c], {
+            'silent': true
+        })), [
+            'a.js',
+            '        1:1  error    Error!',
+            '',
+            '✖ 1 message (1 error, 0 warnings)'
+        ].join('\n'));
+    });
+});
